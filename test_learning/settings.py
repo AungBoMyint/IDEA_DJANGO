f6eebe3b03e6dc7b1301f873a583ce5a59668a18
AungBoMyint <aungbomyint292@gmail.com>
2023-12-23 19:01:03 +0630

    update email configuration

--- test_learning/settings.py
+++ test_learning/settings.py
@@ -209,10 +209,11 @@ DEFAULT_FILE_STORAGE = 'test_learning.storage_backends.PublicMediaStorage'
 # Email Backend SMTP Server
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587 
 EMAIL_HOST_USER = 'truelife787799@gmail.com'
 EMAIL_HOST_PASSWORD = 'rwsbdouaexkfdsqu'
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+DEFAULT_FROM_EMAIL = 'truelife787799@gmail.com'
 #EMAIL_PORT = 2525
 #DEFAULT_FROM_EMAIL = 'learnease@gmail.com'
 #JAZZMIN SETTINGS
