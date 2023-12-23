09fa0c7c16b852013b7dae0d9f04a2c353a8be31
AungBoMyint <aungbomyint292@gmail.com>
2023-12-23 19:21:01 +0630

    try catch in sending email

--- learning/signals/signals.py
+++ learning/signals/signals.py
@@ -3,7 +3,10 @@ from django.dispatch import receiver
 from django.template.loader import render_to_string
 from django.urls import reverse
 from learning.signals import enrollment
+import logging
 from django_rest_passwordreset.signals import reset_password_token_created
+
+logger = logging.getLogger(__name__)
 
 @receiver(reset_password_token_created)
 def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
@@ -18,31 +21,34 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
     :return:
     """
     # send an e-mail to the user
-    context = {
+    try:
+        context = {
         'current_user': reset_password_token.user,
         'username': reset_password_token.user.username,
         'email': reset_password_token.user.email,
         'reset_password_url': "{}?token={}".format(
             instance.request.build_absolute_uri(reverse('password_reset:reset-password-confirm')),
             reset_password_token.key)
-    }
+        }
 
-    # render email text
-    email_html_message = render_to_string('email/password_reset_email.html', context)
-    email_plaintext_message = render_to_string('email/password_reset_email.txt', context)
+        # render email text
+        email_html_message = render_to_string('email/password_reset_email.html', context)
+        email_plaintext_message = render_to_string('email/password_reset_email.txt', context)
 
-    msg = EmailMultiAlternatives(
-        # title:
-        "Password Reset for {title}".format(title="LearningApp"),
-        # message:
-        email_plaintext_message,
-        # from:
-        "noreply@yourdomain.com",
-        # to:
-        [reset_password_token.user.email]
-    )
-    msg.attach_alternative(email_html_message, "text/html")
-    msg.send()
+        msg = EmailMultiAlternatives(
+            # title:
+            "Password Reset for {title}".format(title="LearningApp"),
+            # message:
+            email_plaintext_message,
+            # from:
+            "noreply@yourdomain.com",
+            # to:
+            [reset_password_token.user.email]
+        )
+        msg.attach_alternative(email_html_message, "text/html")
+        msg.send()
+    except Exception as e:
+        logger.error("Error sending email: %s", e)
                  
 
 @receiver(enrollment)
